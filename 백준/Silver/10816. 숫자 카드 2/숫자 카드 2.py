1aa9176d9be3f0323323a28d315655342f143e9e
ImEunhye <85427126+ImEunhye@users.noreply.github.com>
2022-10-04 11:04:22 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 3992 ms, Memory: 112712 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,12 +1,21 @@
-import sys
-input = sys.stdin.readline
+n = int(input())
+cards = sorted(list(map(int,input().split())))
+m = int(input())
+check = list(map(int,input().split()))
 
-
-N = int(input())
-cards = sorted([*map(int, input().split())])
-M = int(input())
-candidate = [*map(int, input().split())]
-
+def b_search(cards,num, left, right):
+    while left<=right :
+        mid = (left+right)//2
+        if cards[mid]>num:
+            right = mid-1
+            return b_search(cards,num,left,right)
+        elif cards[mid]<num:
+            left = mid+1
+            return b_search(cards,num,left,right)
+        elif cards[mid] == num:
+            return count.get(num)
+    return 0
+    
 count = {}
 for card in cards:
     if card in count:
@@ -14,17 +23,5 @@ for card in cards:
     else:
         count[card] = 1
 
-def binarySearch(arr, target, start, end):
-    if start > end:
-        return 0
-    
-    mid = (start + end) // 2
-    if arr[mid] == target:
-        return count.get(target)
-    elif arr[mid] < target:
-        return binarySearch(arr, target, mid+1, end)
-    else:
-        return binarySearch(arr, target, start, mid-1)
-    
-for target in candidate:
-    print(binarySearch(cards, target, 0, len(cards)-1), end=" ")
+for i in range(m):
+    print(b_search(cards,check[i],0,n-1),end = ' ')
