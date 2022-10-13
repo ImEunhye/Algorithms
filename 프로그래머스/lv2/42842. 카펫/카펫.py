af4ef38bc86af30ad7947bb5891b67e425a0c682
ImEunhye <85427126+ImEunhye@users.noreply.github.com>
2022-10-13 11:17:40 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,22 +1,39 @@
-def solution(brown, yellow):
-    list1 = []
-    count = 0
+# def solution(brown, yellow):
+#     list1 = []
+#     count = 0
     
-    for wid in range(3,2500):
-        for hei in range(3,2500):
-            if (wid*hei) == (brown+yellow):
-                list1.append([wid,hei])
+#     for wid in range(3,2500):
+#         for hei in range(3,2500):
+#             if (wid*hei) == (brown+yellow):
+#                 list1.append([wid,hei])
                 
-    for li in list1:
-        bro = li[0]*2 + li[1]*2 - 4
-        yel = (li[0]-2) * (li[1]-2)
+#     for li in list1:
+#         bro = li[0]*2 + li[1]*2 - 4
+#         yel = (li[0]-2) * (li[1]-2)
         
-        if (yel == yellow) and (bro == brown):
-            break
-        count += 1
+#         if (yel == yellow) and (bro == brown):
+#             break
+#         count += 1
         
-    if list1[count][0] < list1[count][1]:
-        return [list1[count][1], list1[count][0]]
+#     if list1[count][0] < list1[count][1]:
+#         return [list1[count][1], list1[count][0]]
     
-    return  list1[count]
+#     return  list1[count]
  
+import math
+def solution(brown, yellow):
+    a = brown + yellow
+    li = []
+    for i in range(1,a+1) :
+        if (a / i) % 1 == 0 :
+            li.append(i)
+ 
+    for j in range(math.ceil(len(li)/2)):
+        width = max(li[j], li[len(li)-j-1])
+        height = min(li[j], li[len(li)-j-1])
+   
+        brown1 =  width*2 + height*2 - 4
+        yellow1 = (width-2) * (height-2)
+
+        if (brown == brown1) and (yellow == yellow1):
+            return [width,height]
