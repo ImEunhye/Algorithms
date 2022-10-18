2ffaf5bd8a3894083826167a19592718bcc7401b
ImEunhye <85427126+ImEunhye@users.noreply.github.com>
2022-10-18 10:40:10 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,20 +1,39 @@
 def solution(n, arr1, arr2):
     answer = []
     
-    for i in range(len(arr1)):
-        bin_1 = bin(arr1[i]).split('b')[1]
-        bin_2 = bin(arr2[i]).split('b')[1]
-        if len(bin_1) < n : bin_1 = '0'*(n-len(bin_1)) + bin_1
-        if len(bin_2) < n : bin_2 = '0'*(n-len(bin_2)) + bin_2
-        arr1[i] = bin_1
-        arr2[i] = bin_2
+    for num1,num2 in zip(arr1,arr2):
+        bin1 = list(bin(num1))[2:]#이진법으로 변환 후 리스트로 저장
+        bin2 = list(bin(num2))[2:]
+        
+        if len(bin1)<n:#자릿수를 맞춤
+            for i in range(n-len(bin1)):
+                bin1.insert(i,'0')
+        if len(bin2)<n:
+            for i in range(n-len(bin2)):
+                bin2.insert(i,'0')
+        
+        str1 =''
+        for b1, b2 in zip(bin1,bin2):
+            if b1 == '1' or b2 == '1':
+                str1 += '#'
+            else: 
+                str1 += ' '
+        answer.append(str1)
+        
+#     for i in range(len(arr1)):
+#         bin_1 = bin(arr1[i]).split('b')[1]
+#         bin_2 = bin(arr2[i]).split('b')[1]
+#         if len(bin_1) < n : bin_1 = '0'*(n-len(bin_1)) + bin_1
+#         if len(bin_2) < n : bin_2 = '0'*(n-len(bin_2)) + bin_2
+#         arr1[i] = bin_1
+#         arr2[i] = bin_2
     
-    for i in range(len(arr1)):
-        ans = ''
-        for j in range(n):
-            if (arr1[i][j] == '1' or arr2[i][j] == '1') : char_ = '#'
-            else: char_ = ' '
-            ans += char_
-        answer.append(ans)           
+#     for i in range(len(arr1)):
+#         ans = ''
+#         for j in range(n):
+#             if (arr1[i][j] == '1' or arr2[i][j] == '1') : char_ = '#'
+#             else: char_ = ' '
+#             ans += char_
+#         answer.append(ans)           
         
     return answer
